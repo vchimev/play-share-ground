eb69843ce6ab951b1f55ae6d3ff865ead4aaf6d4
Vasil Chimev <vasil.chimev@gmail.com>
2019-02-27 11:50:48 +0200

    feat(appCodeShared): login comp

--- appCodeShared/src/app/app.module.tns.ts
+++ appCodeShared/src/app/app.module.tns.ts
@@ -4,7 +4,7 @@ import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-
+import { LoginComponent } from './login/login.component';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from 'nativescript-angular/forms';
@@ -16,6 +16,7 @@ import { HomeComponent } from './home/home.component';
   declarations: [
     AppComponent,
     HomeComponent,
+    LoginComponent,
   ],
   imports: [
     NativeScriptModule,
--- appCodeShared/src/app/app.routes.ts
+++ appCodeShared/src/app/app.routes.ts
@@ -1,15 +1,20 @@
 import { Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   {
-      path: '',
-      redirectTo: '/home',
-      pathMatch: 'full',
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
   },
   {
-      path: 'home',
-      component: HomeComponent,
+    path: 'home',
+    component: HomeComponent,
   },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
 ];
